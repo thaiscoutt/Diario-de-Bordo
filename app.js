d34f69fd6a44bff2bde44551c0dc0e68b35eb5a8
thaiscoutt <thaiscoutinhoteixeira@gmail.com>
2024-09-07 21:01:34 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,54 @@
+function pesquisar() {
+    // Obtém a seção HTML onde os resultados serão exibidos
+    let section = document.getElementById("resultados-pesquisa");
+
+    let campoPesquisa = document.getElementById("campo-pesquisa").value
+
+    // se campoPesquisa for uma string sem nada
+    if (!campoPesquisa) {
+        section.innerHTML = "<p>Nada foi encontrado. Você precisa digitar um local de destino.</p>"
+        return 
+    }
+
+    campoPesquisa = campoPesquisa.toLowerCase()
+
+    // Inicializa uma string vazia para armazenar os resultados
+    let resultados = "";
+    let titulo = ""; 
+    let descricao = "";
+    let clima = "";
+    let custo = "";
+    let tags = "";
+
+    // Itera sobre cada dado da lista de dados
+    for (let dado of dados) {
+        titulo = dado.titulo.toLowerCase()
+        descricao = dado.descricao.toLowerCase()
+        clima = dado.descricao.toLowerCase()
+        custo = dado.descricao.toLowerCase()
+        tags = dado.tags.toLowerCase()
+
+        // se titulo includes campoPesquisa
+        if (titulo.includes(campoPesquisa) || descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+            // cria um novo elemento
+            resultados += `
+            <div class="item-resultado">
+                <h2>
+                    <a href="#" target="_blank">${dado.titulo}</a>
+                </h2>
+                <p class="descricao-meta">${dado.descricao}</p>
+                <p class="descricao-meta">${dado.clima}</p>
+                <p class="descricao-meta">${dado.custo}</p>
+                <a href=${dado.link} target="_blank">Mais informações</a>
+            </div>
+        `;
+        }
+    }
+
+    if (!resultados) {
+        resultados = "<p>Nada foi encontrado</p>"
+    }
+
+    // Atribui os resultados gerados à seção HTML
+    section.innerHTML = resultados;
+}
---
+++ dados.js
@@ -0,0 +1,230 @@
+let dados = [
+    {
+        titulo: "Yellowstone National Park, EUA",
+        descricao: "Descrição: Um dos primeiros parques nacionais do mundo, famoso por suas paisagens vulcânicas, gêiseres e vida selvagem.",
+        clima: "Clima: Continental, com verões quentes e invernos frios e nevados.",
+        custo: "Custo médio diário: R$ 1.500 - R$ 2.500 (incluindo hospedagem em parques, alimentação e atividades).",
+        link: "https://www.youtube.com/shorts/VmhYn83I6c0?feature=share",
+        tags: "América do Norte, America do Norte, USA, Estados Unidos, Estados Unidos da América, Parque Nacional"
+    },
+
+    {
+        titulo: "Haida Gwaii, Canadá",
+        descricao: "Descrição: Um arquipélago remoto com florestas temperadas, praias isoladas e rica cultura indígena Haida.",
+        clima: "Clima: Oceânico, com verões frescos e invernos chuvosos.",
+        custo: "Custo médio diário: R$ 1.500 - R$ 2.500 (incluindo transporte marítimo, hospedagem em lodges e atividades).",
+        link: "https://youtu.be/OS4jJKezOYM",
+        tags: "Canada, Canadá, America do Norte, América do Norte"
+    },
+    
+    {
+        titulo: "Great Basin National Park, EUA",
+        descricao: "Descrição: Um parque com paisagens desérticas, montanhas, cavernas e a árvore mais antiga do mundo.",
+        clima: "Clima: Desértico, com verões quentes e secos e invernos frios com neve.",
+        custo: "Custo médio diário: R$ 700 - R$ 1.000 (incluindo hospedagem em parques, alimentação e atividades).",
+        link: "https://youtu.be/fXIruBaCezo",
+        tags: "América do Norte, America do Norte, USA, Estados Unidos, Estados Unidos da América, Parque Nacional"
+    },
+
+    {
+        titulo: "White Sands National Park, EUA",
+        descricao: "Descrição: Um deserto de gesso com dunas brancas que se estendem por quilômetros, criando um cenário surreal e único.",
+        clima: "Clima: Desértico, com verões quentes e invernos amenos.",
+        custo: "Custo médio diário: R$ 800 - R$ 1.200 (incluindo hospedagem em parques, alimentação e atividades).",
+        link: "https://youtu.be/tSruWf7iaQM",
+        tags: "deserto branco, América do Norte, America do Norte, USA, Estados Unidos, Estados Unidos da América, Parque Nacional"
+    },
+
+    {
+        titulo: "Chapada dos Veadeiros, Goiás",
+        descricao: "Descrição: Um dos maiores parques nacionais do Brasil, com paisagens exuberantes, cachoeiras, cânions e formações rochosas únicas.",
+        clima: "Clima: Tropical de altitude, com verões quentes e úmidos e invernos mais amenos.",
+        custo: "Custo médio diário: R$ 500 - R$ 800 (incluindo hospedagem em pousadas, alimentação e atividades).",
+        link: "https://youtu.be/TEXSn2St_Zw", 
+        tags: "natureza, ecoturismo, brasil, america do sul, américa do sul"
+      },
+
+      {
+        titulo: "Anavilhanas, Amazonas",
+        descricao: "Descrição: Um arquipélago com mais de 400 ilhas fluviais na Amazônia, oferecendo paisagens exuberantes, fauna e flora riquíssimas.",
+        clima: "Clima: Equatorial, quente e úmido durante todo o ano.",
+        custo: "Custo médio diário: R$ 1.200 - R$ 2.000 (incluindo transporte, hospedagem em flutuantes e atividades).",
+        link: "https://youtu.be/h-S0XW6-ZLo", 
+        tags: "amazonia, natureza, ecoturismo, brasil, america do sul, américa do sul"
+      },
+
+      {
+        titulo: "Serra da Canastra, Minas Gerais",
+        descricao: "Descrição: Um dos mais importantes parques nacionais brasileiros, conhecido pela nascente histórica do Rio São Francisco e por suas paisagens exuberantes, com cachoeiras, cânions e campos rupestres.",
+        clima: "Clima: Tropical de altitude, com verões quentes e úmidos e invernos mais amenos.",
+        custo: "Custo médio diário: R$ 500 - R$ 800 (incluindo hospedagem em pousadas, alimentação e atividades).",
+        link: "https://youtu.be/DdFmKZXdOW4",
+        tags: "natureza, ecoturismo, brasil, america do sul, américa do sul"
+      },
+
+      {
+        titulo: "Alter do Chão, Pará",
+        descricao: "Descrição: Conhecida como o Caribe Brasileiro, oferece praias de água doce, rios cristalinos e florestas exuberantes.",
+        clima: "Clima: Equatorial, quente e úmido durante todo o ano.",
+        custo: "Custo médio diário: R$ 600 - R$ 1.000 (incluindo hospedagem em pousadas, alimentação e atividades).",
+        link: "https://youtu.be/Hn89IFD4ECU", 
+        tags: "natureza, ecoturismo, brasil, praia de agua doce, america do sul, américa do sul"
+      },
+
+      {
+        titulo: "Salar de Uyuni, Bolívia",
+        descricao: "Descrição: O maior salar do mundo, com paisagens surrealistas e reflexos que criam um efeito de espelho.",
+        clima: "Clima: Desértico, com grandes variações de temperatura entre o dia e a noite.",
+        custo: "Custo médio diário: R$ 500 - R$ 1.000 (incluindo transporte, hospedagem em albergues e alimentação).",
+        link: "https://youtu.be/8U1Xug7eIN8",
+        tags: "deserto de sal, sal, america do sul, américa do sul, bolivia"
+      },
+
+      {
+        titulo: "Valle de la Luna, Chile",
+        descricao: "Descrição: Um deserto com paisagens lunares, formações rochosas e dunas gigantes, oferecendo um visual único.",
+        clima: "Clima: Desértico, com grandes variações de temperatura entre o dia e a noite.",
+        custo: "Custo médio diário: R$ 300 - R$ 500 (incluindo transporte, hospedagem em albergues e alimentação).",
+        link: "https://youtu.be/DyvS-K8MI6A", 
+        tags: "chile, deserto, lua, america do sul, américa do sul"
+      },
+    
+      {
+        titulo: "Colônia do Sacramento, Uruguai",
+        descricao: "Descrição: Uma cidade histórica com arquitetura colonial, ruas de paralelepípedos e um charme europeu.",
+        clima: "Clima: Subtropical, com verões quentes e úmidos e invernos amenos.",
+        custo: "Custo médio diário: R$ 400 - R$ 700 (incluindo hospedagem em hotéis, alimentação e atividades).",
+        link: "https://youtu.be/KdPx2jRBB8A", 
+        tags: "uruguai, america do sul, américa do sul"
+      },
+    
+      {
+        titulo: "Salar de Atacama, Chile",
+        descricao: "Descrição: Um deserto de sal com paisagens surrealistas, lagunas coloridas e vulcões ativos.",
+        clima: "Clima: Desértico, com noites frias e dias quentes.",
+        custo: "Custo médio diário: R$ 600 - R$ 1.000 (incluindo transporte, hospedagem em albergues e atividades).",
+        link: "https://youtu.be/LnmxKuknXvc", 
+        tags: "chile, deserto, atacama, america do sul, américa do sul"
+      },
+
+      {
+        titulo: "Semuc Champey, Guatemala",
+        descricao: "Descrição: Um paraíso escondido com piscinas naturais de água turquesa cristalina formadas pelo rio Cahabón. A beleza cênica é surreal, com cachoeiras e florestas exuberantes.",
+        clima: "Clima: Tropical, quente e úmido durante todo o ano.",
+        Custo: "Custo médio diário: R$ 500 - R$ 1.000 (incluindo transporte local, alimentação e atividades aquáticas).",
+        link: "https://www.youtube.com/shorts/GSPTbGQN4Fg?feature=share", 
+        tags: "natureza, aventura, cachoeiras, rio, america central, américa central"
+      },
+
+      {
+        titulo: "Cenotes de Yucatán, México",
+        descricao: "Descrição: Formações naturais de água doce, cristalina e de beleza surreal, perfeitas para nadar, mergulhar e explorar cavernas. Uma experiência única em contato com a natureza.",
+        clima: "Clima: Tropical, quente e úmido durante todo o ano.",
+        Custo: "Custo médio diário: R$ 500 - R$ 1.200 (incluindo transporte, ingressos para os cenotes, equipamentos de mergulho e alimentação).",
+        link: "https://youtu.be/SLs7IKwU1Kc", 
+        tags: "natureza, mergulho, cavernas, água doce, Yucatán, américa central, america central"
+      },
+
+      {
+        titulo: "Balos, Grécia",
+        descricao: "Uma lagoa paradisíaca com águas turquesas e areia branca, cercada por falésias. Um verdadeiro paraíso para quem busca relaxar e apreciar a natureza.",
+        clima: "Mediterrâneo, com verões quentes e secos e invernos amenos.",
+        custo: "Custo médio diário: R$ 800 - R$ 1.500 (incluindo transporte, alimentação e atividades aquáticas).",
+        link: "https://youtu.be/xrK8nnH9R-k", 
+        tags: "praia, lagoa, natureza, Grécia, Creta"
+      },
+
+      {
+        titulo: "Hallstatt, Áustria",
+        descricao: "Uma vila alpina charmosa com casas coloridas, um lago cristalino e montanhas majestosas. Um destino perfeito para quem busca tranquilidade e beleza natural.",
+        clima: "Continental, com verões amenos e invernos frios e nevados.",
+        custo: "Custo médio diário: R$ 1.000 - R$ 2.000 (incluindo hospedagem, alimentação e atividades como caminhadas e esqui).",
+        link: "https://youtu.be/FG7K9J356HU", 
+        tags: "alpes, vila, lago, Áustria, inverno"
+      },
+
+      {
+        titulo: "Colmar, França",
+        descricao: "Uma charmosa cidade medieval com canais e casas coloridas, conhecida como a 'Pequena Veneza'.",
+        clima: "Temperado oceânico, com verões amenos e invernos frios e úmidos.",
+        custo: "Custo médio diário: R$ 800 - R$ 1.500 (incluindo hospedagem, alimentação e atividades).",
+        link: "https://www.youtube.com/shorts/xUb_ON4_0Hs?feature=share", 
+        tags: "cidade medieval, canais, França, arquitetura, romance"
+      },
+
+      {
+        titulo: "Reykjavík, Islândia",
+        descricao: "A capital islandesa oferece uma experiência única, com paisagens vulcânicas, geleiras e a aurora boreal.",
+        clima: "Subpolar oceânico, com invernos frios e verões frescos.",
+        custo: "Custo médio diário: R$ 1.200 - R$ 2.000 (incluindo hospedagem, alimentação e atividades).",
+        link: "https://youtu.be/j80T3sbTePM", 
+        tags: "Islândia, aurora boreal, vulcão, geleiras, natureza"
+      },
+
+      {
+        titulo: "Kotor, Montenegro",
+        descricao: "Descrição: Uma cidade medieval cercada por muralhas, localizada em um fiorde. Seu charme histórico e a vista panorâmica do mar Adriático são de tirar o fôlego.",
+        clima: "Clima: Mediterrâneo, com verões quentes e secos e invernos amenos.",
+        custo: "Custo médio diário: R$ 800 - R$ 1.500 (incluindo hospedagem, alimentação e passeios de barco).",
+        link: "https://youtu.be/rwOHOEOEdAY", 
+        tags: "cidade medieval, fiorde, Montenegro, mar Adriático, história"
+      },
+
+      {
+        titulo: "Sossusvlei, Namíbia",
+        descricao: "Descrição: Um deserto de dunas vermelhas gigantescas, um dos maiores do mundo. A paisagem surreal e a vida selvagem adaptada ao ambiente árido tornam o lugar único.",
+        clima: "Clima: Desértico, quente e seco durante o dia, com noites frias.",
+        custo: "Custo médio diário: R$ 1.200 - R$ 2.000 (incluindo hospedagem em lodges, safáris e atividades).",
+        link: "https://youtu.be/lJbKFjMyrRU", 
+        tags: "deserto, dunas, Namíbia, África, natureza"
+      },
+      {
+        titulo: "Ilhas Comores",
+        descricao: "Descrição: Um arquipélago no Oceano Índico com praias paradisíacas, vulcões ativos e uma rica cultura. Um destino ideal para quem busca tranquilidade e aventura.",
+        clima: "Clima: Tropical, quente e úmido durante todo o ano.",
+        custo: "Custo médio diário: R$ 800 - R$ 1.500 (incluindo hospedagem, alimentação e atividades aquáticas).",
+        link: "https://youtu.be/cH91Z6uU_to", 
+        tags: "ilhas, praia, vulcão, Oceano Índico, Comores, áfrica, africa"
+      },
+      {
+        titulo: "Dallol, Etiópia",
+        descricao: "Descrição: Uma paisagem vulcânica surreal, com cores vibrantes e formações geológicas únicas. Um dos lugares mais quentes e inóspitos da Terra.",
+        clima: "Clima: Desértico, extremamente quente e seco.",
+        custo: "Custo médio diário: R$ 1.500 - R$ 2.500 (incluindo expedições guiadas e permissões).",
+        link: "https://www.youtube.com/shorts/5ExOkQKI8Z0?feature=share", 
+        tags: "vulcão, paisagem surreal, Etiópia, África, aventura"
+      },
+      {
+        titulo: "Ilha de Palawan, Filipinas",
+        descricao: "Descrição: Uma ilha paradisíaca com praias de areia branca, águas cristalinas e uma biodiversidade incrível. Um destino ideal para mergulho, snorkeling e trilhas ecológicas.",
+        clima: "Clima: Tropical, quente e úmido durante todo o ano.",
+        custo: "R$ 1.000 - R$ 1.800 (incluindo hospedagem, alimentação e atividades).",
+        link: "Custo médio diário: https://youtu.be/CnOedsCmups", 
+        tags: "Filipinas, ilha, praia, mergulho, natureza, asia, ásia"
+      },
+      
+      {
+        titulo: "Ilha de Jeju, Coreia do Sul",
+        descricao: "Descrição: Uma ilha vulcânica com paisagens dramáticas, praias de areia preta e cavernas de lava. Um destino ideal para relaxar e explorar a natureza.",
+        clima: "Clima: Subtropical, com verões quentes e úmidos e invernos frios.",
+        custo: "Custo médio diário: R$ 1.200 - R$ 2.000 (incluindo hospedagem, alimentação e atividades).",
+        link: "https://www.youtube.com/shorts/bKJQNUROsm0?feature=share", 
+        tags: "Coreia do Sul, ilha vulcânica, praia, natureza, cavernas, asia, ásia"
+      },
+      {
+        titulo: "Pamukkale, Turquia",
+        descricao: "Descrição: Uma paisagem surreal com terraços de travertino branco, formados por águas termais. Um destino único para relaxar em piscinas naturais e explorar ruínas antigas.",
+        clima: "Clima: Mediterrâneo, com verões quentes e secos e invernos amenos.",
+        custo: "Custo médio diário:  $ 900 - R$ 1.600 (incluindo hospedagem, alimentação e entrada em piscinas termais).",
+        link: "https://youtu.be/VaAY9NuctMY", 
+        tags: "Turquia, travertino, águas termais, ruínas, história, asia, ásia"
+      },
+      {
+        titulo: "Ilha Norfolk, Austrália",
+        descricao: "Descrição: Um território australiano no Oceano Pacífico Sul, conhecido por suas praias de areia branca, falésias dramáticas e uma rica história. Um destino ideal para relaxar e explorar a natureza.",
+        clima: "Clima: Subtropical, com verões quentes e úmidos e invernos amenos.",
+        custo: "Custo médio diário: R$ 1.200 - R$ 2.000 (incluindo hospedagem, alimentação e atividades).",
+        link: "https://youtu.be/vJOvIeptkIs", 
+        tags: "Austrália, ilha, praia, falésias, história, oceania"
+      },
+];
